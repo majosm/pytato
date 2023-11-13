885c290406d3945ece93517fe7e52b32c759bc32
Matthew Smith <mjsmith6@illinois.edu>
2023-11-13 13:35:36 -0600

    fix clone_for_callee arguments in _NamedCallResultReplacerPostConcatenate

--- pytato/transform/calls.py
+++ pytato/transform/calls.py
@@ -259,7 +259,8 @@ class _NamedCallResultReplacerPostConcatenate(CopyMapper):
         super().__init__()
 
     @memoize_method
-    def clone_for_callee(self: _SelfMapper) -> _SelfMapper:
+    def clone_for_callee(
+            self: _SelfMapper, function: FunctionDefinition) -> _SelfMapper:
         raise AssertionError("Control should not reach here."
                              " Call clone_with_new_call_on_stack instead.")
 
@@ -826,7 +827,8 @@ class _ConcatabilityCollector(CachedWalkMapper):
         self.ary_to_concatenatability[key] = concatenatability
 
     @memoize_method
-    def clone_for_callee(self: _SelfMapper) -> _SelfMapper:
+    def clone_for_callee(
+            self: _SelfMapper, function: FunctionDefinition) -> _SelfMapper:
         raise AssertionError("Control should not reach here."
                              " Call clone_with_new_call_on_stack instead.")
 
